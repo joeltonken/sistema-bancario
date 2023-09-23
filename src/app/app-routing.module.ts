de40e657a8bb55bae4c7275f9d2ec547fb2c30e2
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-09-22 21:31:21 -0300

    Project created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { CadastroClienteComponent } from
+  './pages/cliente/cadastro-cliente/cadastro-cliente.component';
+import { ListagemClienteComponent } from
+  './pages/cliente/listagem-cliente/listagem-cliente.component';
+const routes: Routes = [
+  {
+    path: 'cliente',
+    children: [
+      {
+        path: 'novo',
+        component: CadastroClienteComponent
+      },
+      {
+        path: 'editar/:id',
+        component: CadastroClienteComponent
+      },
+      {
+        path: '',
+        component: ListagemClienteComponent,
+      },
+    ]
+  },
+  {
+    path: '',
+    component: ListagemClienteComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './shared/material/material.module';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { ClienteModule } from "./pages/cliente/cliente.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [provideNgxMask()],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MaterialModule,
+        NgxMaskDirective,
+        NgxMaskPipe,
+        ClienteModule
+    ]
 })
 export class AppModule { }
